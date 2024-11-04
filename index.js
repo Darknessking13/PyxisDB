961d10fdacaab803369872fd2fffc279457ddc66
KOUSHIK.OG <154724597+iscordian@users.noreply.github.com>
2024-11-04 07:08:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// index.js
-const PyxisDB = require('./pyxisdb');
-
-const pyx = {
-    connect: async (url) => {
-        try {
-            const ping = await PyxisDB.connect(url);
-            return pyx;
-        } catch (error) {
-            console.error('Failed to connect to PyxisCloud:', error);
-            throw error;
-        }
-    },
-    schema: (...args) => PyxisDB.schema(...args),
-    model: (...args) => PyxisDB.model(...args),
-};
-
-module.exports = pyx;
+/*
+No need for this file
+*/
